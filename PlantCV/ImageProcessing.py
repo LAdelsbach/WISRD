0a47c367fe31d2d20d5d8a6eda29c260848649e0
LAdelsbach <lukeadelsbach@gmail.com>
2023-10-02 13:45:08 -0700

    more betterer

--- PlantCV/ImageProcessing.py
+++ PlantCV/ImageProcessing.py
@@ -1,111 +1,8 @@
-
-#
-# import cv2
-# import numpy as np
-#
-# # Replace with the actual file path to your image
-# image_path = "image3.jpg"
-#
-# # Load your image
-# original_image = cv2.imread(image_path)
-#
-# # Convert the image to the HSV color space
-# hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
-#
-# # Define lower and upper bounds for green in HSV
-# lower_green = np.array([20, 20, 20])
-# upper_green = np.array([35, 255, 255])
-#
-# # Create a mask for green pixels
-# green_mask = cv2.inRange(hsv_image, lower_green, upper_green)
-#
-# # Apply the mask to the original image
-# green_objects = cv2.bitwise_and(original_image, original_image, mask=green_mask)
-#
-# # Find contours in the mask
-# contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-# # Initialize a list to store pixel counts for each plant
-# plant_pixel_counts = []
-#
-# # Loop through each detected contour (plant) and calculate pixel count
-# for contour in contours:
-#     pixel_count = cv2.contourArea(contour)
-#     plant_pixel_counts.append(pixel_count)
-#
-#     # Draw the contour on the masked image (green_objects)
-#     cv2.drawContours(green_objects, [contour], -1, (0, 255, 0), 2)
-#
-# # Print pixel counts for each plant
-# for i, pixel_count in enumerate(plant_pixel_counts):
-#     if pixel_count > 1000:
-#         print(f"Plant {i + 1} Pixel Count: {pixel_count}")
-#
-# # Display the original image with contours on the masked image
-# cv2.imshow("Original Image with Contours", green_objects)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# #
-# import cv2
-# import numpy as np
-#
-# # Replace with the actual file path to your image
-# image_path = "image3.jpg"
-#
-# # Load your image
-# original_image = cv2.imread(image_path)
-#
-# # Convert the image to the HSV color space
-# hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
-#
-# # Define lower and upper bounds for green in HSV
-# lower_green = np.array([20, 20, 20])
-# upper_green = np.array([35, 255, 255])
-#
-# # Create a mask for green pixels
-# green_mask = cv2.inRange(hsv_image, lower_green, upper_green)
-#
-# # Apply the mask to the original image
-# masked_image = cv2.bitwise_and(original_image, original_image, mask=green_mask)
-#
-# # Find contours in the mask
-# contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-# # Initialize a list to store bounding boxes
-# bounding_boxes = []
-#
-# # Define a padding value to make the bounding boxes larger
-# padding = 10  # Adjust this value to make the boxes larger or smaller
-#
-# # Loop through each detected contour (plant)
-# for contour in contours:
-#     # Find the bounding rectangle around the plant
-#     x, y, w, h = cv2.boundingRect(contour)
-#
-#     # Expand the bounding rectangle by adding padding
-#     x -= padding
-#     y -= padding
-#     w += 2 * padding
-#     h += 2 * padding
-#
-#     bounding_boxes.append((x, y, x + w, y + h))
-#
-# # Combine overlapping bounding boxes
-# combined_boxes, _ = cv2.groupRectangles(bounding_boxes, groupThreshold=1, eps=0.2)
-#
-# # Draw the combined bounding boxes (overlapping and larger than 10 pixels) on the masked image
-# for (x, y, x2, y2) in combined_boxes:
-#     cv2.rectangle(masked_image, (x, y), (x2, y2), (0, 255, 0), 2)
-#
-# # Display the original image with combined bounding rectangles
-# cv2.imshow("Original Image with Combined Bounding Rectangles", masked_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 import cv2
 import numpy as np
 
 # Replace with the actual file path to your image
-image_path = "image3.jpg"  # Change this to your image file path
+image_path = "image2.jpg"  # Change this to your image file path
 
 # Load your image
 frame = cv2.imread(image_path)
@@ -160,7 +57,7 @@ for contour in contours:
     area = cv2.contourArea(contour)
 
     # Filter out small noise by setting a minimum area threshold
-    if area > 100:
+    if area > 1000:
         # Calculate the centroid of the contour
         M = cv2.moments(contour)
         if M["m00"] > 0:
@@ -181,6 +78,20 @@ for contour in contours:
                 # Create a new plant
                 plant_areas.append(area)
                 plant_centroids.append(centroid)
+
+# ...[previous code]...
+
+# Sort plant_centroids and plant_areas based on y-coordinates and then x-coordinates
+sorted_indices = sorted(range(len(plant_centroids)), key=lambda k: (plant_centroids[k][1], plant_centroids[k][0]))
+sorted_plant_centroids = [plant_centroids[i] for i in sorted_indices]
+sorted_plant_areas = [plant_areas[i] for i in sorted_indices]
+
+# Print the areas in order
+for idx, area in enumerate(sorted_plant_areas):
+    print(f"Area {idx+1}: {area} pixels")
+
+# ...[remaining code]...
+
 
 # Draw the outlines of the merged plant contours on the original image
 outline_frame = frame.copy()
