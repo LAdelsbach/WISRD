c308478d928ba70128490a283cb3e0220997bd06
LAdelsbach <lukeadelsbach@gmail.com>
2024-01-17 10:15:53 -0800

    CreatingLIFI

---
+++ LightBluetooth/Input.cpp
@@ -0,0 +1,81 @@
+int photoPin = A0;
+
+
+ int middle = 200;
+  int length;
+String totalString = "";
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting");
+
+}
+
+void loop() {
+  if(analogRead(photoPin)>middle){
+    longon();
+    shortoff();
+    length = 0;
+    for(int i = 0; i < 7; i++){
+      int light = analogRead(photoPin);
+      if(light > middle){
+        longon();
+        shortoff();
+        totalString += "1";
+        Serial.println("1");
+        length += pow(2, 6-i);
+      }
+      else{
+        longon();
+        shortoff();
+        totalString += "0";
+        Serial.println("0");
+
+      }
+    }
+    for(int i = 0; i < length; i ++){
+      int light = analogRead(photoPin);
+      if(light > middle){
+        longon();
+        shortoff();
+        totalString += "1";
+        Serial.println("1");
+
+      }
+      else{
+        shortoff();
+        totalString += "0";
+        Serial.println("0");
+
+      }
+    }
+     Serial.print("Length" + length);
+
+  }
+  //int light = analogRead(photoPin);
+  ///Serial.println(light);
+ // delay(100);
+} 
+
+
+void shortoff(){
+      long int t1 = millis();
+      while (millis() - t1 <= 200){
+
+      }
+      return;
+  }
+
+   void longon(){
+      long int t1 = millis();
+      while(millis() - t1 <= 2000){
+
+      }
+      return;
+  }
+   void longoff(){
+      long int t1 = millis();
+      while(millis() - t1 <= 2000){
+
+      }
+      return;
+  }
---
+++ LightBluetooth/Output.cpp
@@ -0,0 +1,68 @@
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(13, OUTPUT);
+
+	Serial.begin(9600);
+
+  //20 miiseconds
+long int t1 = millis();
+      digitalWrite(13, LOW); 
+     
+     //so that we get to wait a lil
+      while (millis() - t1 <= 2000){
+
+      }
+
+  String payload = "10000011010";
+
+  int totlength = payload.length();
+  int restlength = 0; 
+  for(int i = 1; i < 8; i ++){
+    if(payload.substring(i, i+1).equals("1")){
+      restlength += pow(2, 7-i);
+    }
+  }
+
+	Serial.println(restlength);
+  for(int i = 0; i < restlength+8; i ++){
+      if(payload.substring(i, 1+ i).equals("1")){
+          longon();
+      }
+      else{
+          longoff();
+      }
+      shortoff();
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void shortoff(){
+      long int t1 = millis();
+      digitalWrite(13, LOW); 
+      while (millis() - t1 <= 200){
+
+      }
+      return;
+  }
+
+   void longon(){
+      long int t1 = millis();
+      digitalWrite(13, HIGH); 
+      while(millis() - t1 <= 2000){
+
+      }
+      digitalWrite(13, LOW); 
+      return;
+  }
+   void longoff(){
+      long int t1 = millis();
+      digitalWrite(13, LOW); 
+      while(millis() - t1 <= 2000){
+
+      }
+      return;
+  }
